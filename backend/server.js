6ff3d320e9dcc2e888fa93cc2a09731891307955
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-09 17:42:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');       hjb flsjhbv
+const dotenv = require('dotenv');       hjb flsjh
 
 // Load env vars
 dotenv.config();
