7f414a7856e425a1d7950810b03a4e98c2735fd3
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-13 19:34:48 +0530

    Update generateQR.js

--- backend/utils/generateQR.js
+++ backend/utils/generateQR.js
@@ -3,7 +3,7 @@ const QRCode = require('qrcode');
 
 const generateQR = async (data) => {
   try {
-    const qrCode = await QRCode.toDataURL(data);        etgergve
+    const qrCode = await QRCode.toDataURL(data);        etgerg
     return qrCode;
   } catch (error) {
     console.error('QR generation error:', error);
