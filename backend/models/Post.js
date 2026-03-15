3be71d267467b12b3b3a2f9a095b507e6874273b
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-15 17:43:37 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -6,7 +6,7 @@ const postSchema = new mongoose.Schema({
     ref: 'User',
     required: true
   },
-  content: {             rtdgd
+  content: {             rtd
     type: String,
     required: true
   },
