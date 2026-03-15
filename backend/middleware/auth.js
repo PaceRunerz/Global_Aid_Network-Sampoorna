e183d16c64f213120fb26d3b64863b2e50325193
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-15 17:41:15 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,7 @@
 // middleware/auth.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
- erfsrvferfv
+ erfsrvfer
 const protect = async (req, res, next) => {
   let token;
 
